369c2ba6befb437d3885f83bbbca90c64c72b53a
ChenluJi <chenlu.ji@columbia.edu>
2016-12-18 17:02:27 -0500

    final report

---
+++ lib/scrip for report.py
@@ -0,0 +1,18 @@
+%matplotlib inline
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from collections import Counter
+import pydotplus 
+from io import StringIO
+from IPython.display import display, Image
+from sklearn.externals.six import StringIO
+import random
+import seaborn as sns
+import time
+
